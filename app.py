f7ada2e86b5b2d5c6bf4fe1a98b0c9d155f7621e
jackson <jackson.dobson2026@gbwl.org>
2026-02-09 17:42:25 +0000

    svae

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask import render_template
+
+# Create an instance of Flask
+app = Flask(__name__)
+
+# Function that returns content
+# using the app route decorator to map the URL  
+
+@app.route("/")
+def index():
+        return render_template("index.html")      
